a27e0793db1b6853e901911e627aa9dfe20fbd68
Gusev Andrey <GusevAndrey1988@gmail.com>
2021-10-22 15:03:06 +0300

    add include areas

---
+++ local/templates/main_template/include_areas/footer_site_info_inc.php
@@ -0,0 +1,2 @@
+<h1 style="font-family: 'Herr Von Muellerhoff';color: #ccc;font-weight:300;text-align: center;margin-bottom:0;margin-top:0;line-height:1.4;font-size: 46px;">Moschino</h1>
+<a target="blank" href="https://www.wowthemes.net/">&copy; Moschino - Free HTML Template by WowThemes.net</a>
---
+++ local/templates/main_template/include_areas/site_title_inc.php
@@ -0,0 +1,2 @@
+<h1 class="site-title"><a href="/" rel="home">Moschino</a></h1>
+<h2 class="site-description">Minimalist Portfolio HTML Template</h2>
